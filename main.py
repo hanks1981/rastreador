b1141a17610437a17f8916febad8cfa06a452efd
hanks1981 <32703591+hanks1981@users.noreply.github.com>
2023-06-13 11:11:47 +0200

    update 1306

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class App:
     SoloMaliciosas = None;
     IpUnica = True;
     Analizando = False;
+    ListaInaddrArpa = None;
+    Invertida = None;
+  
 
     def __init__(self, root):
         # Create the main window
@@ -24,6 +27,7 @@ class App:
         root.geometry(alignstr)
         root.configure(bg='#0aa')
         root.resizable(width=False, height=False)
+        
       
         #API_KEY
         self.EtiquetaApi=tk.Label(root)
@@ -75,11 +79,21 @@ class App:
         self.BotonBuscarIP["text"] = "Check"
         self.BotonBuscarIP.place(x=270,y=40,width=70,height=30)
         self.BotonBuscarIP["command"] = self.Busca_IP
-        
-        
-        
-       
-        
+
+        self.RadioButtonInAddrArpa = tk.Checkbutton(root)
+        # self.RadioButtonInAddrArpa["bg"] = "#efefef"
+        ft = tkFont.Font(family='Times',size=10)
+        self.RadioButtonInAddrArpa["font"] = ft
+        self.RadioButtonInAddrArpa["fg"] = "#333333"
+        self.RadioButtonInAddrArpa["text"] = "In-Addr.arpa"
+        self.RadioButtonInAddrArpa["offvalue"] = "0"
+        self.RadioButtonInAddrArpa["onvalue"] = "1"
+        self.RadioButtonInAddrArpa.place(x=350,y=40,width=130,height=30)
+        self.RadioButtonInAddrArpa["command"] = self.pruebaradiobutton
+
+      
+
+   
         #LISTA DE IPs
         
         self.EtiquetaIPLista= tk.Label()
@@ -89,9 +103,7 @@ class App:
         self.EtiquetaIPLista["bg"] = "#0aa"
         self.EtiquetaIPLista["justify"] = "left"
         self.EtiquetaIPLista["text"] = "Multiple Ips"
-        self.EtiquetaIPLista.place(x=20,y=90,width=200,height=30)
-        
-        
+        self.EtiquetaIPLista.place(x=20,y=90,width=200,height=30) 
     
         self.EligeArchivoBoton=tk.Button()
         ft = tkFont.Font(family='Times',size=10)
@@ -100,29 +112,15 @@ class App:
         self.EligeArchivoBoton["justify"] = "center"
         self.EligeArchivoBoton["text"] = "Choise file"
         self.EligeArchivoBoton.place(x=20,y=120,width=200,height=62)
-        # self.EligeArchivoBoton["bg"] = "#e4cc52"
-        # self.EligeArchivoBoton.configure( fg="#31dc2b")
         self.EligeArchivoBoton["command"] = self.Elige_Archivo
-        
-        # self.NombreArchivoTexto= tk.Label()
-        # ft = tkFont.Font(family='Times',size=10)
-        # self.NombreArchivoTexto["font"] = ft
-        # self.NombreArchivoTexto["fg"] = "#000"
-        # self.NombreArchivoTexto["bg"] = "#fff"
-        # self.NombreArchivoTexto["justify"] = "left"
-        # self.NombreArchivoTexto["text"] = "Sin cargar archivo"
-        # self.NombreArchivoTexto.place(x=20,y=200,width=200,height=25)
-        
-        
+  
         self.Rastrea=tk.Button()
         ft = tkFont.Font(family='Times',size=10)
         self.Rastrea["font"] = ft
         self.Rastrea["fg"] = "#000"
-        # self.Rastrea["bg"] = "#333"
         self.Rastrea["justify"] = "center"
         self.Rastrea["text"] = "Check list"
         self.Rastrea.place(x=20,y=200,width=200,height=40)
-        
         self.Rastrea["command"] = self.Rastrea_AbuseIP
         
         
@@ -197,6 +195,11 @@ class App:
     def Cambia_Estado_Etiqueta_exportadas(self):
         self.EtiquetaExportado["bg"] = "#A2FF33"
 
+    def pruebaradiobutton(self):
+        print("radiobuton activo")
+        self.Invertida = not self.Invertida
+        print("radiobuton activo",self.Invertida)
+
         
 
     def Cierra_Ventana(self):
@@ -207,6 +210,10 @@ class App:
         filepath = filedialog.askopenfilename()
         with open(filepath, 'r', encoding='utf-8') as file:
             self.Lista = file.read()
+            # self.ListaInaddrArpa = self.Lista
+        # self.Lista = re.findall(r'[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.(in\-addr\.arpa)', self.ListaInaddrArpa)
+        # if Aux == "in-addr.arpa":
+        #     self.Invertida()
         self.Lista = re.findall(r'[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}', self.Lista)
         self.Lista = list(set(self.Lista))
         if self.Lista != []:
@@ -216,11 +223,12 @@ class App:
         self.VentanaResultado.insert(tk.END, '--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
         
         for self.ip in self.Lista:
+             print("esto es el self",self.Lista)
              self.VentanaResultado.configure(justify="left")
              self.VentanaResultado.insert(tk.END, f' SIN ANALIZAR -  {self.ip}    ')
              self.VentanaResultado.insert(tk.END, '------------------------------------------------------')
             
-        
+    
         
     def Rastrea_AbuseIP(self):
        #borramos lista anterior
@@ -239,7 +247,7 @@ class App:
             try:
                 response = requests.get(f'https://api.abuseipdb.com/api/v2/check',
                                         params={'ipAddress': self.ip, 'maxAgeInDays': 90},
-                                        headers={'key': "85ab2690d87e8423af50b6ec2f948f7f8af224636a75ccaff7c1c5bb9bb3b6557a7e9f3ed43f7499"}, timeout=5)
+                                        headers={'key': "6d504eb54fa446701a31679bcc42b2e09861920a67b1f410c43efefd29e756f75d9ccf2dec75b9fa"}, timeout=5)
                 self.data = response.json()
             except requests.exceptions.RequestException as e:
                 self.ErrorConexionAPi()
@@ -261,7 +269,7 @@ class App:
         try:
             response = requests.get(f'https://api.abuseipdb.com/api/v2/check',
                                     params={'ipAddress': Only_IP, 'maxAgeInDays': 90},
-                                     headers={'key': "85ab2690d87e8423af50b6ec2f948f7f8af224636a75ccaff7c1c5bb9bb3b6557a7e9f3ed43f7499"}, timeout=5)
+                                     headers={'key': "6d504eb54fa446701a31679bcc42b2e09861920a67b1f410c43efefd29e756f75d9ccf2dec75b9fa"}, timeout=5)
             self.data = response.json()
             if self.data:
                self.BusquedaUnica.delete(0, tk.END)
@@ -349,8 +357,6 @@ class App:
         self.VentanaResultado.insert(tk.END, f' Riesgo: Bajo - {self.data["data"]["ipAddress"]} - Score: {self.data["data"]["abuseConfidenceScore"]} - isp: {self.data["data"]["isp"]}')
         self.VentanaResultado.insert(tk.END, '-------------------------------------------------------------------------------------------------------------------------------------------------------------')
         
-        
-        
     def DameCriticas(self):
         print("---------DameCriticas--------------")
         print("data->", self.data)
@@ -362,130 +368,6 @@ class App:
         print("-----------------------")
         
    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-        
-        
-    
-
-    #     # Create the Open File button
-    #     open_file_button = tk.Button(root, text="Choose File", command=self.open_file)
-    #     open_file_button.pack()
-
-    #     # Create the Search IPs button
-    #     search_ips_button = tk.Button(root, text="Search Abusedb", command=self.search_ips_button_clicked)
-    #     search_ips_button.pack()
-
-    #     # Create the IP list box
-    #     IP_LIST_BOX = tk.Listbox(root, height=20, width=35)
-    #     IP_LIST_BOX.pack()
-
-    #     # Create the Save to File button
-    #     save_to_file_button = tk.Button(root, text="Save to File", command=self.save_to_file)
-    #     save_to_file_button.pack()
-
-    #     # Create the Close button
-    #     close_button = tk.Button(root, text="Close", command=self.close_button_clicked)
-    #     close_button.pack()
-
-
-    # def save_api_key():
-    #     """
-    #     Save the API key
-    #     """
-    #     # App.api_key = API_KEY_ENTRY.get()
-    #     # API_KEY_WINDOW.destroy()
-    #     print("save api key ")
-
-
-    # def enter_api_key():
-    #     """
-    #     Create a window for the user to enter their API key
-    #     """
-    #     global API_KEY_WINDOW
-    #     API_KEY_WINDOW = tk.Toplevel(root)
-    #     API_KEY_WINDOW.geometry("250x100")
-    #     API_KEY_WINDOW.title("API KEY")
-    #     api_key_label = tk.Label(API_KEY_WINDOW, text="Enter your Abuseipdb API key:")
-    #     api_key_label.pack()
-    #     global API_KEY_ENTRY
-    #     API_KEY_ENTRY = tk.Entry(API_KEY_WINDOW)
-    #     API_KEY_ENTRY.pack()
-    #     save_api_key_button = tk.Button(API_KEY_WINDOW, text="Save", command=save_api_key)
-    #     save_api_key_button.pack()
-
-
-    # # Function to open a file.
-    # def open_file(self):
-    #     """
-    #     Open a file
-    #     """
-    #     filepath = filedialog.askopenfilename()
-    #     global IPS
-    #     with open(filepath, 'r', encoding='utf-8') as file:
-    #         IPS = file.read()
-    #     IPS = re.findall(r'[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}', IPS)
-    #     IPS = list(set(IPS))
-
-
-    # def search_ips_button_clicked(self):
-    #     """
-    #     Search the IPs in the file
-    #     """
-    #     # Clear the previous IP list
-    #     IP_LIST_BOX.delete(0, tk.END)
-    #     for ip in IPS:
-    #         try:
-    #             response = requests.get(f'https://api.abuseipdb.com/api/v2/check',
-    #                                     params={'ipAddress': ip, 'maxAgeInDays': 90},
-    #                                     headers={'Key': "App.api_key"}, timeout=5)
-    #             data = response.json()
-    #         except requests.exceptions.RequestException as e:
-    #             IP_LIST_BOX.insert(tk.END, f'{ip} - API request error')
-    #             return
-    #         if data["data"]["abuseConfidenceScore"] >= 50:
-    #             IP_LIST_BOX.insert(tk.END, f'{ip} - IP flagged as suspicious')
-
-
-    # def save_to_file(self):
-    #     """
-    #     Save the flagged IPs to a file
-    #     """
-    #     # Ask user where they want to save the file
-    #     filepath = filedialog.asksaveasfilename(defaultextension=".txt",
-    #                                             filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
-
-    #     # Open the file for writing and write the flagged IPs to it
-    #     with open(filepath, "w", encoding='utf-8') as file:
-    #         for ip in IP_LIST_BOX.get(0, tk.END):
-    #             if "suspicious" in ip:
-    #                 file.write(ip + "\n")
-    #     # Inform the user that the file was saved
-    #     messagebox.showinfo("Save to File", "File saved successfully!")
-
-
-    # def close_button_clicked(self):
-    #     print("cierra")
-    #     # """
-    #     # Close the main window
-    #     # """
-    #     # # window.destroy()
-
-
 if __name__ == '__main__':
     root = tk.Tk()
     app = App(root)
